214622758b0f03d7be7181a1806df6c657b10c86
Felipe-SO <68606807+Felipe-SO@users.noreply.github.com>
2020-07-21 11:25:41 -0500

    Add files via upload

---
+++ fibonacci_module.py
@@ -0,0 +1,44 @@
+#returns selected term of the fibonacci sequence starting in 1, 1
+def fiboeff(term):
+	count=0
+	x=1
+	y=0
+	z=1
+	while count < term-1:
+		z=x
+		x=x+y
+		y=z
+		count=count+1
+	else:
+		return x
+    
+#prints fibonacci sequence until up to the limit term
+def fiboseq(limit):
+    count=0
+    x=1
+    y=0
+    z=1
+    while count < limit-1:
+        print(x)
+        z=x
+        x=x+y
+        y=z
+        count=count+1
+
+#counts the digits of a certain term of the fibonacci sequence
+def fibodigits(term):
+    return(len(str(fiboeff(term))))
+
+#prints index of fibonacci value lesser than x
+def fibolim(x):
+    y=1
+    while fiboeff(y) < x:
+        y+=1
+    print(y-1)
+    return y-1
+
+#prints prints the index and value of the last fibonacci number lesser than limit
+def fiboless(limit):
+    print(fiboeff(fibolim(limit)))
+    
+
